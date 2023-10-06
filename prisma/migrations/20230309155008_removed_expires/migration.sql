2d862aff248430f3b2373c07ddd3e14a8a2c9bbc
gimnathperera <gimnathperera@gmail.com>
2023-10-06 22:43:38 +0530

    config: migrate mysql to mongo db

--- prisma/migrations/20230309155008_removed_expires/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `expires` on the `RefreshToken` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `RefreshToken` DROP COLUMN `expires`;
